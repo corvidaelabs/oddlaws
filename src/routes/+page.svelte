3cd26af2d03fe9c729603cad17ce23eec89a7674
Zachary Corvidae <zach@sneakycrow.dev>
2025-03-12 22:18:46 -0700

    Enable Skeleton UI Framework (#11)
    * Enable skeleton ui
    
    * Remove push deployment for staging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to sneakycrow's oddlaws sandbox</h1>
+<p>I use this website to test different data applications for oddlaws</p>
