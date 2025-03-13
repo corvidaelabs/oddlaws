8649f034152aaaa84bae457b7710c97f8f59afe6
Zachary Corvidae <zach@sneakycrow.dev>
2025-03-13 08:25:05 -0700

    Add login/logout buttons

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+	import { LogOut, LogIn } from 'lucide-svelte';
+
 	let { user } = $props();
 </script>
 
@@ -10,11 +12,15 @@
 			<div class="user">
 				<span>{user.name}</span>
 				<form method="post" action="?/logout" use:enhance>
-					<button class="btn preset-filled-primary-500">Sign out</button>
+					<button class="btn preset-filled-primary-500">
+						Sign out <LogOut size={16} />
+					</button>
 				</form>
 			</div>
 		{:else}
-			<a href="/login" type="button" class="btn preset-filled-primary-500">Login</a>
+			<a href="/login" type="button" class="btn preset-filled-primary-500">
+				Login <LogIn size={16} />
+			</a>
 		{/if}
 	{/snippet}
 	<span>Title</span>
