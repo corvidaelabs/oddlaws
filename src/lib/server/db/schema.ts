6f33cef0bce00ed567003254031efab035fb4811
Zachary Corvidae <zach@sneakycrow.dev>
2025-03-16 22:00:50 -0700

    Added publishedEvents table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -35,6 +35,18 @@ export const memberScreenshots = pgTable('member_screenshots', {
 	updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'date' }).notNull()
 });
 
+export const publishedEvents = pgTable('published_events', {
+	id: uuid('id')
+		.default(sql`gen_random_uuid()`)
+		.primaryKey(),
+	title: text('title').notNull(),
+	description: text('description'),
+	startTime: timestamp('start_time', { withTimezone: true, mode: 'date' }).notNull(),
+	endTime: timestamp('end_time', { withTimezone: true, mode: 'date' }).notNull(),
+	createdAt: timestamp('created_at', { withTimezone: true, mode: 'date' }).notNull(),
+	updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'date' }).notNull()
+});
+
 export type Session = typeof session.$inferSelect;
 
 export type User = typeof user.$inferSelect;
@@ -42,3 +54,5 @@ export type User = typeof user.$inferSelect;
 export type PublishedMember = typeof publishedMembers.$inferSelect;
 
 export type MemberScreenshot = typeof memberScreenshots.$inferSelect;
+
+export type PublishedEvent = typeof publishedEvents.$inferSelect;
